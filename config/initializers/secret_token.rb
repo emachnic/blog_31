4c9c821181cdb03bc76fea4f4ec51fa9181a5af1
Engine Yard <engineyard@ENG000857.local>
2011-09-14 15:11:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog31::Application.config.secret_token = 'dc5579b4e76d6f5c2562eeee5147fe2f83ca28387b13cca57c8bae819d48bd8617ed466e51c61672650a5a538979c98a42d1afff3ca282a6510d9fbc15e3bb3e'
